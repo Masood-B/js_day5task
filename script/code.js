5c294cd7f709d9ae7b92a7c2b30eaa6551788782
Masood-B <masoodbasardien123@gmail.com>
2023-05-19 12:03:53 +0200

    first Attempt

---
+++ script/code.js
@@ -0,0 +1,15 @@
+function greet (){
+    console.log(`Hello`)
+}
+greet();
+
+function multiply (numb1, numb2){
+    return (numb1 * numb2);
+
+}
+console.log(multiply(2,4));
+
+function tableofFive (number){
+    return number * 5
+}
+console.log(tableofFive(2))
